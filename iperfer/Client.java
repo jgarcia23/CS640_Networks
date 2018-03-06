4eeae3cbfcb044d09350ebe365e835dd3d12447b
Joseluis Garcia <32144609+jgarcia23@users.noreply.github.com>
2018-03-05 20:37:55 -0600

    Update Client.java

--- iperfer/Client.java
+++ iperfer/Client.java
@@ -4,6 +4,9 @@ import java.util.*;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.*;
+/**
+ * @author Joseluis Garcia
+ */
 public class Client {
 
     private String hostname;
